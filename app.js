7158fc674ad2d4cb158930c9390298a849b83860
beelai88 <b.emma.lai@gmail.com>
2016-04-27 17:39:12 -0400

    complete express config

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 var express = require( 'express' );
 var app = express(); // creates an instance of an express application
 
+
+app.use(function (req, res, next) {
+
+	console.log(req.url); 
+	console.log(req.method);
+	next();
+});
+
+app.use('/special', function (req, res, next) {
+	res.send("You've reached the special page");
+	next();
+});
+
 app.get('/', function(req, res) {
 	res.send('Welcome!');
-})
+});
+
+app.get('/news', function(req, res) {
+	res.send('Here is the news:');
+});
 
 app.listen(3000, function() {
 	console.log('server listening...');
