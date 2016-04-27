dbe8f6c5fe7a25e6e6cd6e906a3a726e8f9435c1
beelai88 <b.emma.lai@gmail.com>
2016-04-27 18:23:49 -0400

    swig added

--- app.js
+++ app.js
@@ -1,6 +1,36 @@
 var express = require( 'express' );
 var app = express(); // creates an instance of an express application
+var swig = require('swig');
+swig.setDefaults({ cache: false });
 
+app.engine('html', swig.renderFile);
+
+app.set('view engine', 'html');
+
+app.set('views', __dirname + '/views');
+
+
+var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+
+app.use('/views', function(req, res, next) {
+	res.render( 'index', {title: 'Hall of Fame', people: people}, function(err, html) {
+		res.send(html);
+	});	
+})
+
+
+var locals = {
+    title: 'An Example',
+    people: [
+        { name: 'Gandalf'},
+        { name: 'Frodo' },
+        { name: 'Hermione'}
+    ]
+};
+
+swig.renderFile(__dirname + '/views/index.html', locals, function (err, output) {
+    console.log(output);
+});
 
 app.use(function (req, res, next) {
 
